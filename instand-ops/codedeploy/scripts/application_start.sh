8e86a4935e2670b6b709fe170962300b5e1e8dec
fmaj7 <dingguanghao@gmail.com>
2015-09-20 16:34:24 -0700

    Install dependencies at deployment time

--- instand-ops/codedeploy/scripts/application_start.sh
+++ instand-ops/codedeploy/scripts/application_start.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+set -e
 /opt/wdr/env/instand/instand-service/target/stork/bin/instand-service --start
 
 PWD=$(PWD)
 cd /opt/wdr/env/instand/instand-mobile
+sudo yum install nodejs npm --enablerepo=epel
+sudo npm install -g cordova ionic
 ionic serve --address 0.0.0.0 --port 8100
 cd $PWD
