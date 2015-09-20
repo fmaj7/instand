fd820e46a7fead7764d673d4b239d37ad9311ba2
fmaj7 <dingguanghao@gmail.com>
2015-09-20 16:14:04 -0700

    Add script to serve mobile site on deploying

--- instand-ops/codedeploy/scripts/application_start.sh
+++ instand-ops/codedeploy/scripts/application_start.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
 /opt/wdr/env/instand/instand-service/target/stork/bin/instand-service --start
+
+PWD=$(PWD)
+cd /opt/wdr/env/instand/instand-mobile
+ionic serve --address 0.0.0.0 --port 8100
+cd $PWD
