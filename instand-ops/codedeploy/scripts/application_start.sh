29f9aea1bccfe6ae97a441765ac6385a925bc2a7
ericjy <eric@MacBook-Pro.local>
2015-09-19 22:29:44 -0700

    Run Ninja server at background

--- instand-ops/codedeploy/scripts/application_start.sh
+++ instand-ops/codedeploy/scripts/application_start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -Dninja.port=9000 -jar /opt/wdr/env/instand/instand-service/target/instand-service-1.0-SNAPSHOT.jar
+java -Dninja.port=9000 -jar /opt/wdr/env/instand/instand-service/target/instand-service-1.0-SNAPSHOT.jar &
