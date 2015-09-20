db118b3cef10953b77e0bfb261d56d11eb0f86ae
fmaj7 <dingguanghao@gmail.com>
2015-09-20 16:51:12 -0700

    Try it without sudo...

--- instand-ops/codedeploy/scripts/application_start.sh
+++ instand-ops/codedeploy/scripts/application_start.sh
@@ -4,7 +4,7 @@ set -e
 
 PWD=$(pwd)
 cd /opt/wdr/env/instand/instand-mobile
-sudo yum install nodejs npm --enablerepo=epel
-sudo npm install -g cordova ionic
+yum install nodejs npm --enablerepo=epel
+npm install -g cordova ionic
 ionic serve --address 0.0.0.0 --port 8100
 cd $PWD
