650fb689ed59348c22ad2cb342f2ffa7928667df
ericjy <ericjy@users.noreply.github.com>
2015-09-26 13:15:21 -0700

    Install Java 8 and remove Java 7

---
+++ instand-ops/codedeploy/scripts/before_install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+JAVA_VER=$(java -version 2>&1 | sed 's/java version "\(.*\)\.\(.*\)\..*"/\1\2/; 1q')
+if [[ "$JAVA_VER" -ge 18 ]]; then
+    echo "good, Java is 8 or later"
+else
+    echo "java is too old, reinstalling Java 8"
+    yum remove java-1.7.0-openjdk
+    yum install java-1.8.0
+fi
