b270a84bcadf0cacbf6b927d3e931fb81a787eec
fmaj7 <dingguanghao@gmail.com>
2015-10-13 22:36:49 -0700

    Attempt to start ionic serve

--- instand-ops/codedeploy/scripts/application_start.sh
+++ instand-ops/codedeploy/scripts/application_start.sh
@@ -2,9 +2,10 @@
 set -e
 /opt/wdr/env/instand/instand-service/target/stork/bin/instand-service --start
 
+# Temporary solution to start ionic serve
 PWD=$(pwd)
 cd /opt/wdr/env/instand/instand-mobile
 yum -y install nodejs npm --enablerepo=epel
 npm install -g cordova ionic
-ionic serve --address 0.0.0.0 --port 8100
+screen -d -m -L ionic serve --address 0.0.0.0 --port 8100 --nolivereload
 cd $PWD
--- instand-ops/codedeploy/scripts/application_stop.sh
+++ instand-ops/codedeploy/scripts/application_stop.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 /opt/wdr/env/instand/instand-service/target/stork/bin/instand-service --stop
+
+# Kill all screen sessions which should only be used to run ionic serve
+screen -ls | grep pts | cut -d. -f1 | awk '{print $1}' | xargs kill
