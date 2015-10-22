e7f47e1c197e26742e0d6ddcc44461b79c2320e9
ericjy <ericjy@users.noreply.github.com>
2015-10-21 22:28:35 -0700

    Force exit 0 if the kill command has error
    This is an quick attempt to fix the deployment error: https://console.aws.amazon.com/codedeploy/home?region=us-east-1#/deployments/d-BYVQX69YB 
    
    The solution was found here: http://unix.stackexchange.com/questions/118217/chmod-silent-mode-how-force-exit-code-0-in-spite-of-error

--- instand-ops/codedeploy/scripts/application_stop.sh
+++ instand-ops/codedeploy/scripts/application_stop.sh
@@ -2,4 +2,4 @@
 /opt/wdr/env/instand/instand-service/target/stork/bin/instand-service --stop
 
 # Kill all screen sessions which should only be used to run ionic serve
-screen -ls | grep pts | cut -d. -f1 | awk '{print $1}' | xargs kill
+screen -ls | grep pts | cut -d. -f1 | awk '{print $1}' | xargs kill || true
