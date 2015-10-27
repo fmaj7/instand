7362d141d990a867e0e7272a3885f8c0324e2567
fmaj7 <dingguanghao@gmail.com>
2015-10-26 21:29:09 -0700

    #12 Move stop commands to start script

--- instand-ops/codedeploy/scripts/application_start.sh
+++ instand-ops/codedeploy/scripts/application_start.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 set -e
+
+# Stop services
+# The reason to stop services in start script is that CodeDeploy always uses previous version's stop script, which means
+# if there is a bug to fail the stop script, CodeDeploy deployment will forever fail till manual intervention.
+/opt/wdr/env/instand/instand-service/target/stork/bin/instand-service --stop
+
+# Kill screen sessions which should only be used to run ionic serve
+PID=`screen -ls | grep ionic | cut -d. -f1 | awk '{print $1}'`
+if [ -n "${PID}" ] ; then echo $PID | xargs kill; fi
+
+# Start services
 /opt/wdr/env/instand/instand-service/target/stork/bin/instand-service --start
 
 # Temporary solution to start ionic serve
--- instand-ops/codedeploy/scripts/application_stop.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-/opt/wdr/env/instand/instand-service/target/stork/bin/instand-service --stop
-
-# Kill screen sessions which should only be used to run ionic serve
-PID=`screen -ls | grep ionic | cut -d. -f1 | awk '{print $1}'`
-if [ -n "${PID}" ] ; then echo $PID | xargs kill; fi
